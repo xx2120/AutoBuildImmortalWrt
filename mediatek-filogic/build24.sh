f7cb43c651c524895e4b659c1e9ed3bd69187624
xx2120 <121374645+xx2120@users.noreply.github.com>
2025-10-16 16:02:50 +0800

    Update build24.sh

--- mediatek-filogic/build24.sh
+++ mediatek-filogic/build24.sh
@@ -27,12 +27,11 @@ arch aarch64_cortex-a53 15' repositories.conf
 echo "Building for profile: $PROFILE"
 
 echo "Include Docker: $INCLUDE_DOCKER"
+# yml 传入的固件大小 ROOTFS_PARTSIZE
+echo "编译固件大小为: $ROOTFS_PARTSIZE MB"
+
 echo "Create pppoe-settings"
 mkdir -p  /home/build/immortalwrt/files/etc/config
-
-
-# yml 传入的固件大小 ROOTFS_PARTSIZE
-echo "编译固件大小为: $PROFILE MB"
 
 # 创建pppoe配置文件 yml传入pppoe变量————>pppoe-settings文件
 cat << EOF > /home/build/immortalwrt/files/etc/config/pppoe-settings
@@ -102,7 +101,7 @@ fi
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
 echo "$PACKAGES"
 
-make image PROFILE=$PROFILE PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE=$PROFILE
+make image PROFILE=$PROFILE PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_PARTSIZE=$ROOTFS_PARTSIZE
 
 if [ $? -ne 0 ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"
